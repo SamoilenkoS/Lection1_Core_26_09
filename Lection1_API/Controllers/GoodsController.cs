dfbf6caf1ce538ae80f996664a83d83dbe9564f3
Sviatoslav Samoilenko <Sviatoslav_Samoilenko@epam.com>
2022-09-26 21:15:06 +0300

    Add project files.

---
+++ Lection1_API/Controllers/GoodsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lection1_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GoodsController : ControllerBase
+    {
+        public GoodsController()
+        {
+        }
+    }
+}
