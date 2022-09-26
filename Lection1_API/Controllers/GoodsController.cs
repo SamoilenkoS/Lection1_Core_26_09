9daea08b7fb4673dc597acf933ac3a079e97156f
Sviatoslav Samoilenko <Sviatoslav_Samoilenko@epam.com>
2022-09-26 21:34:33 +0300

    Added basic implementation of Goods CRUD methods

--- Lection1_API/Controllers/GoodsController.cs
+++ Lection1_API/Controllers/GoodsController.cs
@@ -11,8 +11,79 @@ namespace Lection1_API.Controllers
     [Route("[controller]")]
     public class GoodsController : ControllerBase
     {
+        private static List<Good> _goods;
+
+        static GoodsController()
+        {
+            _goods = new List<Good>()
+            {
+                new Good
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "First good",
+                    Price = 100.5
+                }
+            };
+        }
+
         public GoodsController()
         {
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_goods);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(Guid id)
+        {
+            var good = _goods.Find(x => x.Id == id);
+            if(good != null)
+            {
+                return Ok(good);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Good good)
+        {
+            good.Id = Guid.NewGuid();
+            _goods.Add(good);
+
+            return Created(good.Id.ToString(), good);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(Guid id, Good good)
+        {
+            good.Id = id;
+            int index = _goods.FindIndex(x => x.Id == id);
+            if (index >= 0)
+            {
+                _goods[index] = good;
+
+                return Ok(good);
+            }
+
+            return NotFound();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            int index = _goods.FindIndex(x => x.Id == id);
+            if (index >= 0)
+            {
+                _goods.RemoveAt(index);
+
+                return NoContent();
+            }
+
+            return NotFound();
         }
     }
 }
